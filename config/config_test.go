216f7a8e7fd560ca127a62f118c1d0b743e76e34
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-28 17:44:24 +0800

    Update test to be more thorough

--- config/config_test.go
+++ config/config_test.go
@@ -8,9 +8,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLoadFromEnvironment(t *testing.T) {
-	os.Setenv(GO_ENV, constants.ENV_DEVELOPMENT)
-	conf, err := LoadFromEnvironment()
-	assert.Nil(t, err)
-	assert.Equal(t, constants.ENV_DEVELOPMENT, conf.Environment)
+func TestLoadFromEnvironment_AppEnvironment(t *testing.T) {
+	t.Run("should return development environment when GO_ENV is development", func(t *testing.T) {
+		os.Setenv(GO_ENV, constants.ENV_DEVELOPMENT)
+		conf, err := LoadFromEnvironment()
+		assert.Nil(t, err)
+		assert.Equal(t, constants.ENV_DEVELOPMENT, conf.Environment)
+	})
+	t.Run("should return production environment when GO_ENV is production", func(t *testing.T) {
+		os.Setenv(GO_ENV, constants.ENV_PRODUCTION)
+		conf, err := LoadFromEnvironment()
+		assert.Nil(t, err)
+		assert.Equal(t, constants.ENV_PRODUCTION, conf.Environment)
+	})
+	t.Run("should return production environment when GO_ENV anything that is not development", func(t *testing.T) {
+		os.Setenv(GO_ENV, "anything")
+		conf, err := LoadFromEnvironment()
+		assert.Nil(t, err)
+		assert.Equal(t, constants.ENV_PRODUCTION, conf.Environment)
+	})
 }
