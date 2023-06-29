e4b8a9e176219891c0fc632c8ddb453a1b2ce8b3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-30 01:29:39 +0800

    Set up database connections

--- utils/constants/db.go
+++ utils/constants/db.go
@@ -1,5 +1,8 @@
 package constants
 
 const (
-	DB_DEFAULT_PORT = 5432
+	// TODO: Fall back to these values if the environment variables are not set
+	DB_DEFAULT_TIMEZONE = "Asia/Singapore"
+	DB_DEFAULT_PORT     = 5432
+	DB_DEFAULT_NAME     = "sa-stories"
 )
