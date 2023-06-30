19238ffd86ea95698fe92b34ee247250d85a8560
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-30 11:32:22 +0800

    Use singleton pattern for logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -13,13 +13,22 @@ type Logger struct {
 	Debug   *log.Logger
 }
 
-func (l *Logger) Setup(output *os.File) {
-	// Info writes logs in the color blue with "INFO: " as prefix
-	l.Info = log.New(output, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile)
-	// Warning writes logs in the color yellow with "WARNING: " as prefix
-	l.Warning = log.New(output, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile)
-	// Error writes logs in the color red with "ERROR: " as prefix
-	l.Error = log.New(output, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
-	// Debug writes logs in the color cyan with "DEBUG: " as prefix
-	l.Debug = log.New(output, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile)
+var l *Logger = nil
+
+func Setup(output *os.File) (l *Logger) {
+	// Singleton pattern
+	if l != nil {
+		return l
+	}
+	l = &Logger{
+		// Info writes logs in the color blue with "INFO: " as prefix
+		Info: log.New(output, "\u001b[34mINFO: \u001B[0m", log.LstdFlags|log.Lshortfile),
+		// Warning writes logs in the color yellow with "WARNING: " as prefix
+		Warning: log.New(output, "\u001b[33mWARNING: \u001B[0m", log.LstdFlags|log.Lshortfile),
+		// Error writes logs in the color red with "ERROR: " as prefix
+		Error: log.New(output, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile),
+		// Debug writes logs in the color cyan with "DEBUG: " as prefix
+		Debug: log.New(output, "\u001b[36mDEBUG: \u001B[0m", log.LstdFlags|log.Lshortfile),
+	}
+	return l
 }
