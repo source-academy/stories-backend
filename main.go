4e3473be4bc838ce0469788648b478952765436d
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-06-30 15:25:24 +0800

    Set up Sentry for crash reporting (#13)
    * Install Sentry dependencies
    
    * Load Sentry DSN into config
    
    * Initialize Sentry on startup
    
    * Update go.mod
    
    * Update example .env file
    
    * Update dependencies
    
    * Only run Sentry in production

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/source-academy/stories-backend/internal/config"
 	"github.com/source-academy/stories-backend/internal/database"
 	"github.com/source-academy/stories-backend/internal/router"
+	"github.com/source-academy/stories-backend/internal/utils/constants"
+)
+
+const (
+	STARTUP_MESSAGE = "Starting server..."
 )
 
 func main() {
@@ -23,6 +30,27 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer database.Close(db)
+
+	// Initialze Sentry configuration
+	// TODO: Migrate logic to routing middleware
+	//       or internal logger package.
+	if conf.Environment == constants.ENV_PRODUCTION {
+		err := sentry.Init(sentry.ClientOptions{
+			Dsn: conf.SentryDSN,
+			// Set TracesSampleRate to 1.0 to capture 100%
+			// of transactions for performance monitoring.
+			// We recommend adjusting this value in production,
+			TracesSampleRate: 1.0,
+		})
+		if err != nil {
+			log.Fatalln("sentry.Init:", err)
+
+		}
+		// Flush buffered events before the program terminates.
+		defer sentry.Flush(2 * time.Second)
+		// Notify that server is starting
+		sentry.CaptureMessage(STARTUP_MESSAGE)
+	}
 
 	// Setup router
 	r := router.Setup(conf)
