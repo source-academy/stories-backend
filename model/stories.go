1ea835279ec325ec148329691d5f49fddf2abd16
gracelimshiern <98506014+gracelimshiern@users.noreply.github.com>
2023-07-05 14:52:41 +0800

    edit story struct

--- model/stories.go
+++ model/stories.go
@@ -1,14 +1,17 @@
 package model
 
 type Story struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+	StoryID      int       `json:"story_id"`
+	UserID       int       `json:"user_id"`
+	StoryContent string    `json:"story_content"`
+	CreatedAt    time.Time `json:"created_at"`
+	DeletedAt    time.Time `json:"deleted_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
 }
 
 var stories = []Story{
-	{ID: 1, Title: "Story 1", Author: "Username1"},
-	{ID: 2, Title: "Story 2", Author: "Username2"},
+	{StoryID: 1, UserID: 1, StoryContent: "Story 1 Content", CreatedAt: time.Now(), DeletedAt: time.Now(), UpdatedAt: time.Now()},
+	{StoryID: 2, UserID: 2, StoryContent: "Story 2 Content", CreatedAt: time.Now(), DeletedAt: time.Now(), UpdatedAt: time.Now()},
 }
 
 func GetAllStories() []Story {
