145a4cb6aeebde4d70af8a398305cf867b8d0e6f
Rena Ng <e0772915@u.nus.edu>
2023-07-05 15:31:29 +0800

    edit users model

--- model/users.go
+++ model/users.go
@@ -1,14 +1,19 @@
 package model
 
+import "time"
+
 type User struct {
-	UserID         int    `json:"user_id"`
-	GithubUsername string `json:"github_username"`
-	GithubID       int    `json:"github_ID"`
+	UserID         int       `json:"user_id"`
+	GithubUsername string    `json:"github_username"`
+	GithubID       int       `json:"github_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	DeletedAt      time.Time `json:"deleted_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 var users = []User{
-	{UserID: 1, GithubUsername: "User 1", GithubID: 1},
-	{UserID: 2, GithubUsername: "User 2", GithubID: 2},
+	{UserID: 1, GithubUsername: "User 1", GithubID: 1, CreatedAt: time.Now(), DeletedAt: time.Now(), UpdatedAt: time.Now()},
+	{UserID: 2, GithubUsername: "User 2", GithubID: 2, CreatedAt: time.Now(), DeletedAt: time.Now(), UpdatedAt: time.Now()},
 }
 
 func GetAllUsers() []User {
