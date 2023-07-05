6778089901a82a5ec6b3a223139a94ae8b7b6105
gracelimshiern <98506014+gracelimshiern@users.noreply.github.com>
2023-07-05 14:25:32 +0800

    update migration files to to support ORM capabilities of GORM.

--- migrations/20230704145116-create_users_table.sql
+++ migrations/20230704145116-create_users_table.sql
@@ -1,10 +1,13 @@
-
 -- +migrate Up
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     github_username TEXT,
     github_ID INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     UNIQUE (user_id)
 );
+
 -- +migrate Down
--- migrations/20230704145138-create_stories_table.sql
+++ migrations/20230704145138-create_stories_table.sql
@@ -4,8 +4,12 @@ CREATE TABLE IF NOT EXISTS stories (
     story_id SERIAL PRIMARY KEY,
     user_id INT,
     story_content TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     UNIQUE (story_id)
 );
+
 
 -- +migrate Down
