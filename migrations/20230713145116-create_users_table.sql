478ceef53becb34f7d5a7c1d57ea65a2f14f9c0d
Rena Ng <e0772915@u.nus.edu>
2023-07-13 15:52:26 +0800

    Remove tigger function in migration files

--- migrations/20230713145116-create_users_table.sql
+++ migrations/20230713145116-create_users_table.sql
@@ -9,24 +9,6 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at updated_at_type
 );
 
--- +migrate StatementBegin
-CREATE OR REPLACE FUNCTION update_users_updated_at_column()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF (NEW.github_username IS DISTINCT FROM OLD.github_username) THEN
-        NEW.updated_at = CURRENT_TIMESTAMP;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER update_users_updated_at_trigger
-    BEFORE UPDATE ON users
-    FOR EACH ROW
-    EXECUTE FUNCTION update_users_updated_at_column();
-
--- +migrate StatementEnd
-
 -- +migrate Down
 
 DROP TRIGGER IF EXISTS update_users_updated_at_trigger ON users;
--- migrations/20230714125055-create_stories_table.sql
+++ migrations/20230714125055-create_stories_table.sql
@@ -9,24 +9,6 @@ CREATE TABLE IF NOT EXISTS stories (
     updated_at updated_at_type
 );
 
--- +migrate StatementBegin
-CREATE OR REPLACE FUNCTION update_stories_updated_at_column()
-    RETURNS TRIGGER AS $$
-BEGIN
-    IF (NEW.story_content IS DISTINCT FROM OLD.story_content) THEN
-        NEW.updated_at = CURRENT_TIMESTAMP;
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER update_stories_updated_at_trigger
-    BEFORE UPDATE ON stories
-    FOR EACH ROW
-    EXECUTE FUNCTION update_stories_updated_at_column();
-
--- +migrate StatementEnd
-
 -- +migrate Down
 
 DROP TRIGGER IF EXISTS update_stories_updated_at_trigger ON stories;
