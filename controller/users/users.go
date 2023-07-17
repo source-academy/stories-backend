052e316374a73c2159e4d5d24414ae5fcc4ca998
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-17 23:35:56 +0800

    Remove DB global variable from users MVC (#44)
    * Rename users' controller methods
    
    * Add function to get DB sesion from request context
    
    * Fix incorrect users list view response
    
    * Remove global DB variable from users model
    
    * Update users' controller

--- controller/users/users.go
+++ controller/users/users.go
@@ -7,37 +7,62 @@ import (
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
 
 	"github.com/source-academy/stories-backend/controller"
+	"github.com/source-academy/stories-backend/internal/database"
 	"github.com/source-academy/stories-backend/model"
 	userparams "github.com/source-academy/stories-backend/params/users"
 	userviews "github.com/source-academy/stories-backend/view/users"
 )
 
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-	users := model.GetAllUsers()
-	controller.EncodeJSONResponse(w, users)
+func HandleList(w http.ResponseWriter, r *http.Request) {
+	// Get DB instance
+	db, err := database.GetDBFrom(r)
+	if err != nil {
+		logrus.Error(err)
+		panic(err)
+	}
+
+	users := model.GetAllUsers(db)
+	controller.EncodeJSONResponse(w, userviews.ListFrom(users))
 }
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
+func HandleRead(w http.ResponseWriter, r *http.Request) {
 	userIDStr := chi.URLParam(r, "userID")
 	userID, err := strconv.Atoi(userIDStr)
 	if err != nil {
 		http.Error(w, "Invalid userID", http.StatusBadRequest)
 		return
 	}
-	user := model.GetUserByID(userID)
-	controller.EncodeJSONResponse(w, userviews.SingleFrom(*user))
+
+	// Get DB instance
+	db, err := database.GetDBFrom(r)
+	if err != nil {
+		logrus.Error(err)
+		panic(err)
+	}
+
+	user := model.GetUserByID(db, userID)
+	controller.EncodeJSONResponse(w, userviews.SingleFrom(user))
 }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
+func HandleCreate(w http.ResponseWriter, r *http.Request) {
 	var params userparams.Create
 	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	userModel := *params.ToModel()
-	model.CreateUser(&userModel)
+
+	// Get DB instance
+	db, err := database.GetDBFrom(r)
+	if err != nil {
+		logrus.Error(err)
+		panic(err)
+	}
+
+	model.CreateUser(db, &userModel)
 	controller.EncodeJSONResponse(w, userviews.SingleFrom(userModel))
 	w.WriteHeader(http.StatusCreated)
 }
