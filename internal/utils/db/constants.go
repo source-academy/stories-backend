d6626509ec8173c8f7a60a5bc2a5b320c6ae26ad
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-18 01:19:50 +0800

    Add comments

--- internal/utils/db/constants.go
+++ internal/utils/db/constants.go
@@ -10,6 +10,8 @@ const (
 	DB_DEFAULT_NAME     = "sa_stories"
 )
 
+// Not used for now
+// TODO: Remove if not used
 func GetOrDefault[T comparable](val T, deflt T) T {
 	var empty T
 	if val == empty {
