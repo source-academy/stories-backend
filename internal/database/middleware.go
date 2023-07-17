80401d1b56c6266a3b1e205f84fdbbbdacb3633b
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-17 22:11:17 +0800

    Use own type for context keys
    Prevents collisions as statically-checked by golangci-lint.

--- internal/database/middleware.go
+++ internal/database/middleware.go
@@ -7,15 +7,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// We define our own type to avoid collisions
+type contextKey string
+
 const (
-	contextKey = "database_context"
+	dbKey contextKey = "database_context"
 )
 
 func MakeMiddlewareFrom(db *gorm.DB) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// Create a new session from the database for each request
-			ctx := context.WithValue(r.Context(), contextKey, db.Session(&gorm.Session{}))
+			ctx := context.WithValue(r.Context(), dbKey, db.Session(&gorm.Session{}))
 			// Inject the new context into the request
 			next.ServeHTTP(w, r.WithContext(ctx))
 		})
