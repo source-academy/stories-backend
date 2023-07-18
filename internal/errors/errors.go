7f4c73a7707e5c3aafc9dbcf1de6d7f53bbeaf11
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-18 10:53:46 +0800

    Create basic ClientError type

---
+++ internal/errors/errors.go
@@ -0,0 +1,8 @@
+package errors
+
+// ClientError is an interface for errors that should be returned to the client.
+// They generally start with a 4xx HTTP status code.
+type ClientError interface {
+	error
+	HTTPStatusCode() int
+}
