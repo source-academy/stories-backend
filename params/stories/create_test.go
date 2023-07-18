a3e1e4f7aae7059d19ce18f8423d093cafb3125f
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-19 00:46:13 +0800

    Adapt users' create params test for stories

---
+++ params/stories/create_test.go
@@ -0,0 +1,23 @@
+package storyparams
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidate(t *testing.T) {
+	t.Run("should do nothing for now", func(t *testing.T) {})
+}
+
+func TestToModel(t *testing.T) {
+	t.Run("should create a story model with the correct values", func(t *testing.T) {
+		params := Create{
+			AuthorID: 1,
+			Content:  "# Hi\n\nThis is a test story.",
+		}
+		model := params.ToModel()
+		assert.Equal(t, params.AuthorID, model.AuthorID)
+		assert.Equal(t, params.Content, model.Content)
+	})
+}
