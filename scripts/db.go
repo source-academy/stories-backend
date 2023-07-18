5b1bc78ff3727fae431a7a1aad613638f7eac0a0
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-18 12:00:24 +0800

    Refactor script
    * Rearranged some lines
    * Reduces code duplication
    * Improves code organisation
    * Improves logical separation

--- scripts/db.go
+++ scripts/db.go
@@ -22,6 +22,10 @@ var (
 	blueSandwich  = color.With(color.Blue, "≡")
 	greenTick     = color.With(color.Green, "✔")
 	yellowChevron = color.With(color.Yellow, "❯")
+
+	migrations = (migrate.FileMigrationSource{
+		Dir: "migrations",
+	})
 )
 
 func main() {
@@ -32,42 +36,28 @@ func main() {
 		panic(err)
 	}
 
-	migrations := (migrate.FileMigrationSource{
-		Dir: "migrations",
-	})
+	var connector func(config.DatabaseConfig) (*gorm.DB, error)
 
-	// TODO: Reduce code duplication
+	// Check for command line arguments
 	flag.Parse()
-	// connect to the DB
-	var d *gorm.DB
-	var db *sql.DB
 	switch flag.Arg(0) {
 	case "drop", "create":
-		d, err = connectAnonDB(*conf.Database)
-		if err != nil {
-			logrus.Errorln(err)
-			panic(err)
-		}
-		defer closeDBConnection(d)
+		connector = connectAnonDB
 	case "migrate", "rollback", "status":
-		d, err := connectDB(*conf.Database)
-		if err != nil {
-			logrus.Errorln(err)
-			panic(err)
-		}
-		defer closeDBConnection(d)
-
-		db, err = d.DB()
-		if err != nil {
-			logrus.Errorln(err)
-			panic(err)
-		}
+		connector = connectDB
 	default:
 		logrus.Errorln("Invalid command")
 		return
 	}
 
-	// handling the command
+	// Connect to the database
+	d, err := connector(*conf.Database)
+	if err != nil {
+		logrus.Errorln(err)
+		panic(err)
+	}
+	defer closeDBConnection(d)
+
 	switch flag.Arg(0) {
 	case "drop":
 		err := dropDB(d, conf.Database)
@@ -84,57 +74,82 @@ func main() {
 		}
 		fmt.Println(greenTick, "Created database:", conf.Database.DatabaseName)
 	case "migrate":
-		var steps int
-		if flag.Arg(1) == "" {
-			steps = defaultMaxMigrateSteps
-		} else if steps, err = strconv.Atoi(flag.Arg(1)); err != nil {
-			logrus.Warningln("Invalid number of steps", err)
-			logrus.Warningf("Defaulting to %d steps\n", defaultMaxMigrateSteps)
-			steps = defaultMaxMigrateSteps
-		}
-		_, err = migrate.ExecMax(db, "postgres", migrations, migrate.Up, steps)
+		db, err := d.DB()
 		if err != nil {
 			logrus.Errorln(err)
 			panic(err)
 		}
-		fmt.Println(greenTick, "Migration complete")
+		migrateDB(db)
 	case "rollback":
-		var steps int
-		if flag.Arg(1) == "" {
-			steps = defaultMaxRollbackSteps
-		} else if steps, err = strconv.Atoi(flag.Arg(1)); err != nil {
-			logrus.Warningln("Invalid number of steps", err)
-			logrus.Warningf("Defaulting to %d steps\n", defaultMaxRollbackSteps)
-			steps = defaultMaxRollbackSteps
-		}
-		_, err = migrate.ExecMax(db, "postgres", migrations, migrate.Down, steps)
+		db, err := d.DB()
 		if err != nil {
 			logrus.Errorln(err)
 			panic(err)
 		}
-		fmt.Println(greenTick, "Rollback complete")
+		rollbackDB(db)
 	case "status":
-		completed, err := migrate.GetMigrationRecords(db, "postgres")
+		db, err := d.DB()
 		if err != nil {
 			logrus.Errorln(err)
 			panic(err)
 		}
-		fmt.Printf(greenTick+" %d migrations previously done:\n", len(completed))
-		for _, c := range completed {
-			fmt.Println("  "+greenTick, c.Id)
-		}
+		statusDB(db)
+	}
+}
 
-		pending, _, err := migrate.PlanMigration(db, "postgres", migrations, migrate.Up, 0)
-		if err != nil {
-			logrus.Errorln(err)
-			panic(err)
-		}
+func migrateDB(db *sql.DB) {
+	var steps int
+	var err error
+	if flag.Arg(1) == "" {
+		steps = defaultMaxMigrateSteps
+	} else if steps, err = strconv.Atoi(flag.Arg(1)); err != nil {
+		logrus.Warningln("Invalid number of steps", err)
+		logrus.Warningf("Defaulting to %d steps\n", defaultMaxMigrateSteps)
+		steps = defaultMaxMigrateSteps
+	}
+	_, err = migrate.ExecMax(db, "postgres", migrations, migrate.Up, steps)
+	if err != nil {
+		logrus.Errorln(err)
+		panic(err)
+	}
+	fmt.Println(greenTick, "Migration complete")
+}
+func rollbackDB(db *sql.DB) {
+	var steps int
+	var err error
+	if flag.Arg(1) == "" {
+		steps = defaultMaxRollbackSteps
+	} else if steps, err = strconv.Atoi(flag.Arg(1)); err != nil {
+		logrus.Warningln("Invalid number of steps", err)
+		logrus.Warningf("Defaulting to %d steps\n", defaultMaxRollbackSteps)
+		steps = defaultMaxRollbackSteps
+	}
+	_, err = migrate.ExecMax(db, "postgres", migrations, migrate.Down, steps)
+	if err != nil {
+		logrus.Errorln(err)
+		panic(err)
+	}
+	fmt.Println(greenTick, "Rollback complete")
+}
+func statusDB(db *sql.DB) {
+	completed, err := migrate.GetMigrationRecords(db, "postgres")
+	if err != nil {
+		logrus.Errorln(err)
+		panic(err)
+	}
+	fmt.Printf(greenTick+" %d migrations previously done:\n", len(completed))
+	for _, c := range completed {
+		fmt.Println("  "+greenTick, c.Id)
+	}
 
-		fmt.Printf(yellowChevron+" Pending %d migrations:\n", len(pending))
-		for _, p := range pending {
-			fmt.Println("  "+yellowChevron, p.Migration.Id)
-		}
-	default:
-		logrus.Errorln("Invalid command")
+	pending, _, err := migrate.PlanMigration(db, "postgres", migrations, migrate.Up, 0)
+	if err != nil {
+		logrus.Errorln(err)
+		panic(err)
+	}
+
+	fmt.Printf(yellowChevron+" Pending %d migrations:\n", len(pending))
+	for _, p := range pending {
+		fmt.Println("  "+yellowChevron, p.Migration.Id)
 	}
 }
