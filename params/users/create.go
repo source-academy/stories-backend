e7aa82ba46b20b759366eb901e25a00927cdc3df
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-21 22:26:32 +0800

    Update user create params

--- params/users/create.go
+++ params/users/create.go
@@ -1,20 +1,51 @@
 package userparams
 
-import "github.com/source-academy/stories-backend/model"
+import (
+	"errors"
+
+	userenums "github.com/source-academy/stories-backend/internal/enums/users"
+	"github.com/source-academy/stories-backend/model"
+)
+
+const (
+	loginParamGitHub = "github"
+	loginParamNUSNET = "luminus" // for legacy reasons
+)
 
 type Create struct {
-	GithubUsername string `json:"githubUsername"`
-	GithubID       int    `json:"githubId"`
+	Username      string `json:"username"`
+	LoginProvider string `json:"provider"`
 }
 
 // TODO: Add some validation
 func (params *Create) Validate() error {
+	// Validate login provider is one of the ones supported
+	switch params.LoginProvider {
+	case loginParamGitHub, loginParamNUSNET:
+		break
+	default:
+		return errors.New("Invalid login provider")
+	}
+
 	return nil
 }
 
 func (params *Create) ToModel() *model.User {
 	return &model.User{
-		GithubUsername: params.GithubUsername,
-		GithubID:       params.GithubID,
+		Username:      params.Username,
+		LoginProvider: convertProvider(params.LoginProvider),
+	}
+}
+
+func convertProvider(provider string) userenums.LoginProvider {
+	switch provider {
+	case loginParamGitHub:
+		return userenums.LoginProviderGitHub
+	case loginParamNUSNET:
+		return userenums.LoginProviderNUSNET
+	default:
+		// Should never happen as we previously validated the provider
+		// in the Validate() function, thus ok to panic
+		panic(errors.New("Illegal path - invalid provider"))
 	}
 }
