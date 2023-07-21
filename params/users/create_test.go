4e3855d1d2662b161827dff4c0cf9e3d94a6dce3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-21 23:24:55 +0800

    Add more user create params tests

--- params/users/create_test.go
+++ params/users/create_test.go
@@ -3,22 +3,53 @@ package userparams
 import (
 	"testing"
 
+	userenums "github.com/source-academy/stories-backend/internal/enums/users"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestValidate(t *testing.T) {
-	t.Run("should do nothing for now", func(t *testing.T) {})
+	t.Run("should ensure username is not empty", func(t *testing.T) {
+		params := Create{
+			Username: "",
+		}
+
+		err := params.Validate()
+		assert.NotNil(t, err)
+	})
+	t.Run("should ensure supported and enabled login provider passes", func(t *testing.T) {
+		params := Create{
+			Username:      "testUsername",
+			LoginProvider: userenums.LoginProviderNUSNET.String(),
+		}
+		err := params.Validate()
+		assert.Nil(t, err)
+	})
+	t.Run("should ensure unsupported login provider fails", func(t *testing.T) {
+		params := Create{
+			Username:      "testUsername",
+			LoginProvider: "invalidProvider",
+		}
+		err := params.Validate()
+		assert.NotNil(t, err)
+	})
+	t.Run("should ensure disabled login provider fails", func(t *testing.T) {
+		params := Create{
+			Username:      "testUsername",
+			LoginProvider: userenums.LoginProviderGitHub.String(),
+		}
+		err := params.Validate()
+		assert.NotNil(t, err)
+	})
 }
 
 func TestToModel(t *testing.T) {
 	t.Run("should create a user model with the correct values", func(t *testing.T) {
 		params := Create{
 			Username:      "testUsername",
-			LoginProvider: "github",
+			LoginProvider: userenums.LoginProviderNUSNET.String(),
 		}
 		model := params.ToModel()
 		assert.Equal(t, params.Username, model.Username)
 		assert.Equal(t, params.LoginProvider, model.LoginProvider.String())
 	})
-	// TODO: Add test for invalid login provider
 }
