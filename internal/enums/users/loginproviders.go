af02215065cb593005ccc8e9cd0a570a062a44e9
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-21 22:36:12 +0800

    Create loginprovider string repr from within enum

--- internal/enums/users/loginproviders.go
+++ internal/enums/users/loginproviders.go
@@ -6,3 +6,13 @@ const (
 	LoginProviderGitHub LoginProvider = iota
 	LoginProviderNUSNET
 )
+
+func (provider LoginProvider) String() string {
+	switch provider {
+	case LoginProviderGitHub:
+		return "github"
+	case LoginProviderNUSNET:
+		return "luminus" // for legacy reasons
+	}
+	return "unknown"
+}
