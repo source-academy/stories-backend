239eddd0cbfdc653e306ad7f606195d688dbdda0
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-25 22:31:23 +0800

    Add user to request context (#65)
    * Create auth session context functions
    
    * Inject session to request context
    
    * Remove Session struct
    
    Inject the authenticated user's ID into the request context directly,
    bypassing the session struct, in order to achieve greater flexibility.
    
    * Rename file from session to context

---
+++ internal/auth/context.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+)
+
+type contextKey string
+
+const (
+	userContextKey contextKey = "auth_context"
+)
+
+// Injects the session into the request context
+func injectUserIDToContext(r *http.Request, userID int) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, userID)
+	return r.WithContext(ctx)
+}
+
+func GetUserIDFrom(r *http.Request) (*int, error) {
+	userID, ok := r.Context().Value(userContextKey).(*int)
+	if !ok {
+		return nil, errors.New("Could not get user ID from request context")
+	}
+	return userID, nil
+}
