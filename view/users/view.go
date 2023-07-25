ef8d6bdcdef4cef2e200a759085b425d2d9d9984
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-25 21:23:11 +0800

    Add names to users (#69)
    * Create migration file
    
    * Update User model
    
    * Update users' params and views

--- view/users/view.go
+++ view/users/view.go
@@ -4,13 +4,19 @@ import "github.com/source-academy/stories-backend/model"
 
 type View struct {
 	ID            uint   `json:"id"`
+	Name          string `json:"name"`
 	Username      string `json:"username"`
 	LoginProvider string `json:"provider"`
 }
 
 func SingleFrom(user model.User) View {
+	name := user.FullName
+	if name == "" {
+		name = user.Username
+	}
 	userView := View{
 		ID:            user.ID,
+		Name:          name,
 		Username:      user.Username,
 		LoginProvider: user.LoginProvider.ToString(),
 	}
