4d395e99b2e4c29493ba25153d63d142e30fafad
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 17:53:40 +0800

    Fix deploy scripts (#71)
    * Add comments
    
    * Limit concurrency and memory usage in containers
    
    Prevents crashes when running on systems with lesser hardware
    capabilities.
    
    * Update git clone URL
    
    * Fix SSL validation not working
    
    Also cleans up apt package lists to reduce final image size.
    
    * Fix hosts error on Linux
    
    * Use environment variables in Docker compose
    
    Also adds the JWKS_ENDPOINT required environment variable.

--- deployment/migrator/entrypoint.sh
+++ deployment/migrator/entrypoint.sh
@@ -2,9 +2,15 @@
 
 set -euo pipefail
 
+# Environment variables for Golang, must match .env.example
 export DB_HOSTNAME="$PG_HOST"
 export DB_USERNAME="$PG_USER"
 export DB_NAME="$PG_DATABASE"
+
+# Limit CPU and memory usage for the migrator
+# to prevent crashing the deployment server.
+export GOMAXPROCS="1"
+export GOMEMLIMIT="256MiB"
 
 while ! pg_isready -q -h "$PG_HOST" -U "$PG_USER"; do
     echo "$(date) - waiting for database to start"
