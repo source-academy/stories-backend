947e277ad0ba0f841a25b9ec6339c12a4d1f4fdf
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 15:06:47 +0800

    Create story permission groups

---
+++ internal/permissiongroups/stories/stories.go
@@ -0,0 +1,26 @@
+package storypermissiongroups
+
+import (
+	"github.com/source-academy/stories-backend/internal/auth/permissions"
+	userpermissions "github.com/source-academy/stories-backend/internal/auth/permissions/users"
+)
+
+func Create() permissions.PermissionGroup {
+	return userpermissions.
+		GetRolePermission(userpermissions.CanCreateStories)
+}
+
+func Read() permissions.PermissionGroup {
+	return userpermissions.
+		GetRolePermission(userpermissions.CanReadStories)
+}
+
+func Update(storyID uint) permissions.PermissionGroup {
+	return permissions.AnyOf{
+		Groups: []permissions.PermissionGroup{
+			userpermissions.
+				GetRolePermission(userpermissions.CanUpdateStories),
+			IsAuthorOf{StoryID: storyID},
+		},
+	}
+}
