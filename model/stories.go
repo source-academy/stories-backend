c57a3494c6550ab1f08fcb1c050fb852c0ae92bd
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-26 10:00:42 +0800

    Return author name in story views (#72)
    * Update Story model and associated methods
    
    Preloads the necessary associations to populate the Author field in the
    model struct.
    
    * Update story list and single views

--- model/stories.go
+++ model/stories.go
@@ -8,13 +8,23 @@ import (
 type Story struct {
 	gorm.Model
 	AuthorID uint
+	Author   User
 	Title    string
 	Content  string
 }
 
+var (
+	preloadAssociations = func(db *gorm.DB) *gorm.DB {
+		return db.Preload("Author")
+	}
+)
+
 func GetAllStories(db *gorm.DB) ([]Story, error) {
 	var stories []Story
-	err := db.Find(&stories).Error
+	err := db.
+		Scopes(preloadAssociations).
+		Find(&stories).
+		Error
 	if err != nil {
 		return stories, database.HandleDBError(err, "story")
 	}
@@ -23,7 +33,10 @@ func GetAllStories(db *gorm.DB) ([]Story, error) {
 
 func GetStoryByID(db *gorm.DB, id int) (Story, error) {
 	var story Story
-	err := db.First(&story, id).Error
+	err := db.
+		Scopes(preloadAssociations).
+		First(&story, id).
+		Error
 	if err != nil {
 		return story, database.HandleDBError(err, "story")
 	}
@@ -31,7 +44,14 @@ func GetStoryByID(db *gorm.DB, id int) (Story, error) {
 }
 
 func CreateStory(db *gorm.DB, story *Story) error {
-	err := db.Create(story).Error
+	err := db.
+		Scopes(preloadAssociations).
+		Create(story).
+		// Get associated Author. See
+		// https://github.com/go-gorm/gen/issues/618 on why
+		// a separate .First() is needed.
+		First(story).
+		Error
 	if err != nil {
 		return database.HandleDBError(err, "story")
 	}
