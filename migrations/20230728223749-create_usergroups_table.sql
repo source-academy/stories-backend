54121524a118f4e3b782bff55f98a4c82992b5c3
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-31 01:43:26 +0800

    Fix usergroups migration file
    * Fix incorrect table name in rollback command
    * Replace type with domain
    * Fix missing type deletion in rollback command

--- migrations/20230728223749-create_usergroups_table.sql
+++ migrations/20230728223749-create_usergroups_table.sql
@@ -1,12 +1,12 @@
 -- +migrate Up
 
-CREATE TYPE role AS ENUM('member', 'moderator', 'admin');
+CREATE DOMAIN role_type ENUM('member', 'moderator', 'admin') NOT NULL DEFAULT 'member';
 
 CREATE TABLE IF NOT EXISTS user_groups (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     group_id INT REFERENCES groups(id),
-    role role NOT NULL DEFAULT 'member',
+    role role_type,
     created_at created_at_type,
     deleted_at  deleted_at_type,
     updated_at updated_at_type
@@ -14,4 +14,6 @@ CREATE TABLE IF NOT EXISTS user_groups (
 
 -- +migrate Down
 
-DROP TABLE IF EXISTS usergroups;
+DROP TABLE IF EXISTS user_groups;
+
+DROP DOMAIN role_type;
