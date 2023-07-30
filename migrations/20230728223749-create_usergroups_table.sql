b157f2c5f53af778fcd503eb38f2a9e8de62055d
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-31 01:52:25 +0800

    Replace domain with type in usergroups migration
    Fixes errors.

--- migrations/20230728223749-create_usergroups_table.sql
+++ migrations/20230728223749-create_usergroups_table.sql
@@ -1,12 +1,12 @@
 -- +migrate Up
 
-CREATE DOMAIN role_type ENUM('member', 'moderator', 'admin') NOT NULL DEFAULT 'member';
+CREATE TYPE role_type AS ENUM ('member', 'moderator', 'admin');
 
 CREATE TABLE IF NOT EXISTS user_groups (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     group_id INT REFERENCES groups(id),
-    role role_type,
+    role role_type NOT NULL DEFAULT 'member',
     created_at created_at_type,
     deleted_at  deleted_at_type,
     updated_at updated_at_type
@@ -16,4 +16,4 @@ CREATE TABLE IF NOT EXISTS user_groups (
 
 DROP TABLE IF EXISTS user_groups;
 
-DROP DOMAIN role_type;
+DROP TYPE role_type;
