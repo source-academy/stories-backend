38175d7545d24d783932e92ac04c96f9a76c2623
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-07-31 23:18:21 +0800

    Create handler for reading self information

--- controller/users/read.go
+++ controller/users/read.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
 	"github.com/source-academy/stories-backend/controller"
+	"github.com/source-academy/stories-backend/internal/auth"
 	"github.com/source-academy/stories-backend/internal/database"
 	apierrors "github.com/source-academy/stories-backend/internal/errors"
 	"github.com/source-academy/stories-backend/model"
@@ -36,5 +37,28 @@ func HandleRead(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 	controller.EncodeJSONResponse(w, userviews.SingleFrom(user))
+	return nil
+}
+
+func HandleReadSelf(w http.ResponseWriter, r *http.Request) error {
+	userID, err := auth.GetUserIDFrom(r)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	// Get DB instance
+	db, err := database.GetDBFrom(r)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+
+	user, err := model.GetUserByID(db, *userID)
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+	controller.EncodeJSONResponse(w, userviews.SummaryFrom(user))
 	return nil
 }
