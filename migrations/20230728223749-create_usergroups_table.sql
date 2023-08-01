f5a1048eae6fec03a2dc6ef489b1f16b038feede
YaleChen299 <proverb299@gmail.com>
2023-08-01 12:02:15 +0800

    change unique index name

--- migrations/20230728223749-create_usergroups_table.sql
+++ migrations/20230728223749-create_usergroups_table.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS user_groups (
     updated_at updated_at_type
 );
 
-CREATE UNIQUE INDEX idx_unique_username_provider ON user_groups (user_id, group_id);
+CREATE UNIQUE INDEX idx_unique_user_group ON user_groups (user_id, group_id);
 
 -- +migrate Down
 
-DROP INDEX idx_unique_username_provider;
+DROP INDEX idx_unique_user_group;
 
 DROP TABLE IF EXISTS user_groups;
 
