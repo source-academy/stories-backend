aa699947bf9ee34b718d99907e5c2897313afb16
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-04 05:08:27 +0800

    Create role validation method

--- internal/enums/groups/role.go
+++ internal/enums/groups/role.go
@@ -14,18 +14,8 @@ const (
 	RoleAdmin     Role = "admin"
 )
 
-// We cannot name it String() because it will conflict with the String() method
-// TODO: remove if not used. might not be needed unless we need to handle the error case
-func (role Role) ToString() string {
-	switch role {
-	case RoleStandard:
-		return "member"
-	case RoleModerator:
-		return "moderator"
-	case RoleAdmin:
-		return "admin"
-	}
-	return "unknown"
+func (role Role) IsValid() bool {
+	return role == RoleStandard || role == RoleModerator || role == RoleAdmin
 }
 
 func IsRoleGreaterThan(role1, role2 Role) bool {
