1e301b7cc63c1587c234024b69a9ac64eff72fb4
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-04 05:05:13 +0800

    Remove unused code

--- internal/enums/groups/role.go
+++ internal/enums/groups/role.go
@@ -23,43 +23,6 @@ func (role Role) ToString() string {
 	return "unknown"
 }
 
-// TODO: remove if not used. since string can be accepted to a role type
-func RoleFromString(role string) (Role, bool) {
-	switch role {
-	case "member":
-		return RoleStandard, true
-	case "moderator":
-		return RoleModerator, true
-	case "admin":
-		return RoleAdmin, true
-	}
-	// We fall back to standard role as default
-	return RoleStandard, false
-}
-
-// Implements the Scanner interface
-// func (role *Role) Scan(value interface{}) error {
-// 	str, ok := value.(string)
-// 	if !ok {
-// 		return errors.New("failed to scan role")
-// 	}
-// 	*role = Role(str)
-// 	return nil
-// }
-
-// // Imokements the Valuer interface
-// func (role Role) Value() (driver.Value, error) {
-// 	switch role {
-// 	case RoleStandard:
-// 		return "member", nil
-// 	case RoleModerator:
-// 		return "moderator", nil
-// 	case RoleAdmin:
-// 		return "admin", nil
-// 	}
-// 	return "member", errors.New("unknown role, fall back to member.")
-// }
-
 func IsRoleGreaterThan(role1, role2 Role) bool {
 	switch role1 {
 	case RoleAdmin:
