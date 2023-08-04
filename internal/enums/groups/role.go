612b4d73821716fa36154f9c1ec44d323c9a0b23
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-04 12:38:12 +0800

    Simplify group roles (#90)
    * Comment out Scanner/Valuer interface methods
    
    * Remove unused code
    
    * Add comment
    
    * Create role validation method

--- internal/enums/groups/role.go
+++ internal/enums/groups/role.go
@@ -1,12 +1,12 @@
 package groupenums
 
-import (
-	"database/sql/driver"
-	"errors"
-)
-
 type Role string
 
+// We use database enums for roles, unlike login providers,
+// hence the string type instead of int.
+//
+// As a side effect, we do not need a RoleFromString function
+// as strings can be directly accepted into Role types.
 const (
 	RoleUnknown   Role = ""
 	RoleStandard  Role = "member"
@@ -14,51 +14,8 @@ const (
 	RoleAdmin     Role = "admin"
 )
 
-// We cannot name it String() because it will conflict with the String() method
-// TODO: remove if not used. might not be needed unless we need to handle the error case
-func (role Role) ToString() string {
-	switch role {
-	case RoleStandard:
-		return "member"
-	case RoleModerator:
-		return "moderator"
-	case RoleAdmin:
-		return "admin"
-	}
-	return "unknown"
-}
-
-// TODO: remove if not used. since string can be accepted to a role type
-func RoleFromString(role string) (Role, bool) {
-	switch role {
-	case "member":
-		return RoleStandard, true
-	case "moderator":
-		return RoleModerator, true
-	case "admin":
-		return RoleAdmin, true
-	}
-	// We fall back to standard role as default
-	return RoleStandard, false
-}
-
-// Implements the Scanner interface
-func (role *Role) Scan(value interface{}) error {
-	*role = Role(value.(string))
-	return nil
-}
-
-// Imokements the Valuer interface
-func (role Role) Value() (driver.Value, error) {
-	switch role {
-	case RoleStandard:
-		return "member", nil
-	case RoleModerator:
-		return "moderator", nil
-	case RoleAdmin:
-		return "admin", nil
-	}
-	return "member", errors.New("unknown role, fall back to member.")
+func (role Role) IsValid() bool {
+	return role == RoleStandard || role == RoleModerator || role == RoleAdmin
 }
 
 func IsRoleGreaterThan(role1, role2 Role) bool {
