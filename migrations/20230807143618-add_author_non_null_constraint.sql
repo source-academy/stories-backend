1eb77ed7e2915aee724595fecc8e6be06b8e4fda
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-07 22:17:56 +0800

    Improve migration file formatting consistency

--- migrations/20230807143618-add_author_non_null_constraint.sql
+++ migrations/20230807143618-add_author_non_null_constraint.sql
@@ -1,7 +1,9 @@
 -- +migrate Up
+
 ALTER TABLE stories
     ALTER COLUMN author_id SET NOT NULL;
 
 -- +migrate Down
+
 ALTER TABLE stories
     ALTER COLUMN author_id DROP NOT NULL;
--- migrations/20230807190029-add_user_group_not_null.sql
+++ migrations/20230807190029-add_user_group_not_null.sql
@@ -1,9 +1,11 @@
 -- +migrate Up
+
 ALTER TABLE user_groups
     ALTER COLUMN user_id SET NOT NULL,
     ALTER COLUMN group_id SET NOT NULL;
 
 -- +migrate Down
+
 ALTER TABLE user_groups
     ALTER COLUMN user_id DROP NOT NULL,
     ALTER COLUMN group_id DROP NOT NULL;
