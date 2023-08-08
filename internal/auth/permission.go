7c2d21afd7451b62aeac3318a0356f8a36c05f94
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-08 21:08:24 +0800

    Update CheckPermissions signature

--- internal/auth/permission.go
+++ internal/auth/permission.go
@@ -1,14 +1,19 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/source-academy/stories-backend/internal/permissions"
 )
 
-func CheckPermissions(r *http.Request, requestedActionPermissions ...permissions.PermissionGroup) (bool, error) {
+func CheckPermissions(r *http.Request, requestedActionPermissions ...permissions.PermissionGroup) error {
 	requiredPermissions := permissions.AllOf{
 		Groups: requestedActionPermissions,
 	}
-	return requiredPermissions.IsAuthorized(r), nil
+	isAuthorized := requiredPermissions.IsAuthorized(r)
+	if !isAuthorized {
+		return errors.New("You are not authorized to perform this action.")
+	}
+	return nil
 }
