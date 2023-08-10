46e2104022cc5e5e930c0676d7ae30f62ab81b9d
YaleChen299 <proverb299@gmail.com>
2023-08-10 16:15:40 +0800

    remove double space

--- migrations/20230810152313-add_course_groups.sql
+++ migrations/20230810152313-add_course_groups.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS course_groups (
     course_id INT PRIMARY KEY,
     group_id INT REFERENCES groups(id) NOT NULL,
     created_at created_at_type,
-    deleted_at  deleted_at_type,
+    deleted_at deleted_at_type,
     updated_at updated_at_type
 );
 
